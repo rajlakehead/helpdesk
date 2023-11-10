4d1f97ea84335e30cfc179e222365151b1ac6cc7
RAJ PATEL <patelraj24365@gmail.com>
2023-11-10 17:21:53 -0500

    user signup created

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,19 @@
+"use client"
 import React from 'react'
+import AuthForm from '../AuthForm'
 
 export default function Login() {
+  
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <main>
+      <h2 className='text-center'>Log in</h2>
+
+      <AuthForm handleSubmit={handleSubmit}/>
+    </main>
   )
 }
