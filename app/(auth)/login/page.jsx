553fed1b794eafeb768f8c7dbbd0d90ee36d3c08
RAJ PATEL <patelraj24365@gmail.com>
2023-11-14 22:53:00 -0500

    login function added

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,12 +1,29 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import AuthForm from '../AuthForm'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
 
 export default function Login() {
+  const [error, setError] = useState()
+  const router = useRouter()
   
   const handleSubmit = async (e, email, password) => {
     e.preventDefault()
-    console.log(email, password)
+    setError('')
+
+    const supabase = createClientComponentClient()
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    })
+
+    if (error){
+      setError(error.message)
+    }
+    if (!error){
+      router.push('/')
+    }
   }
 
   return (
@@ -14,6 +31,10 @@ export default function Login() {
       <h2 className='text-center'>Log in</h2>
 
       <AuthForm handleSubmit={handleSubmit}/>
+
+      {error && (
+        <div className="error">{error}</div>
+      )}
     </main>
   )
 }
