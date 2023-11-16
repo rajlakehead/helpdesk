46089d9114f64d1777cc64c94bb67d329d1586f6
RAJ PATEL <patelraj24365@gmail.com>
2023-11-16 17:19:15 -0500

    dataset connected

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,29 +1,24 @@
 import { NextResponse } from "next/server"
-
-export const dynamic = 'force-dynamic'
-
-export async function GET(){
-    const res = await fetch('http://localhost:4000/tickets')
-
-    const tickets = await res.json()
-
-    return NextResponse.json(tickets, {
-        status: 200
-    })
-}
+import { cookies } from 'next/headers'
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 
 export async function POST(request) {
-    const ticket = await request.json()
-  
-    const res = await fetch('http://localhost:4000/tickets', {
-      method: 'POST',
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(ticket)
+  const ticket = await request.json()
+
+  // get supabase instance
+  const supabase = createRouteHandlerClient({ cookies })
+
+  // get current user session
+  const { data: { session } } = await supabase.auth.getSession()
+
+  // insert the data
+  const { data, error } = await supabase.from('Tickets')
+    .insert({
+      ...ticket,
+      user_email: session.user.email,
     })
-  
-    const newTicket = await res.json()
-  
-    return NextResponse.json(newTicket, {
-      status: 201
-    })
-  }
+    .select()
+    .single()
+
+  return NextResponse.json({ data, error })
+}
